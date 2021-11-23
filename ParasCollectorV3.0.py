cb62b8b3bc88e8320ccbd5f1ea8ca4efcf45348a
1ndex <2309896932@qq.com>
2021-11-23 10:49:42 +0800

    新增json格式响应处理

--- ParasCollectorV3.0.py
+++ ParasCollectorV3.0.py
@@ -28,131 +28,136 @@ print("""
 
 
 class BurpExtender(IBurpExtender, IProxyListener, IExtensionStateListener):
-	def registerExtenderCallbacks(self,callbacks):
-		self._callbacks = callbacks
-		self._helpers = callbacks.getHelpers()
-		self._callbacks.setExtensionName('ParasCollector')
-		# 记录扫描历史请求的位置，启用插件是默认为0
-		self._index = 0
-		self._count = 0
-		self._threadLock = Lock()
-		self._allParas = {}
-		callbacks.registerProxyListener(self)
-		callbacks.registerExtensionStateListener(self)
+    def registerExtenderCallbacks(self,callbacks):
+        self._callbacks = callbacks
+        self._helpers = callbacks.getHelpers()
+        self._callbacks.setExtensionName('ParasCollector')
+        # 记录扫描历史请求的位置，启用插件是默认为0
+        self._index = 0
+        self._count = 0
+        self._threadLock = Lock()
+        self._allParas = {}
+        callbacks.registerProxyListener(self)
+        callbacks.registerExtensionStateListener(self)
 
-		# 在启用插件的时候就扫描一次
-		allHistory = self._callbacks.getProxyHistory()
-		self._end = len(allHistory)
-		start_t = Thread(target=self.getParas, args=(self._end,allHistory))
-		start_t.start()
-		return
+        # 在启用插件的时候就扫描一次
+        allHistory = self._callbacks.getProxyHistory()
+        self._end = len(allHistory)
+        start_t = Thread(target=self.getParas, args=(self._end,allHistory))
+        start_t.start()
+        return
 
-	def processProxyMessage(self, messageIsRequest, messageInfo):
-		# 每1000个请求扫描一次，需要注意的是，发起了1000个请求，history不一定有1000个记录
-		self._count = self._count + 1
-		if(self._count==1000):
-			self._count=0
-			# 获取到的请求是从所有请求的第一个开始的
-			allHistory = self._callbacks.getProxyHistory()
-			self._end = len(allHistory)
-			# 采用线程的方式执行获取参数的行为，避免阻塞请求响应
-			t = Thread(target=self.getParas, args=(self._end,allHistory))
-			t.start()
-			# t.join()
-		
+    def processProxyMessage(self, messageIsRequest, messageInfo):
+        # 每1000个请求扫描一次，需要注意的是，发起了1000个请求，history不一定有1000个记录
+        self._count = self._count + 1
+        if(self._count==1000):
+            self._count=0
+            # 获取到的请求是从所有请求的第一个开始的
+            allHistory = self._callbacks.getProxyHistory()
+            self._end = len(allHistory)
+            # 采用线程的方式执行获取参数的行为，避免阻塞请求响应
+            t = Thread(target=self.getParas, args=(self._end,allHistory))
+            t.start()
+            # t.join()
+        
 
-	def getParas(self,end,allHistory):
-		# 会在线程中被改变的数需要在锁里面获取跟改变，如果以形参方式传入会导致数据错乱，不会改变的数据应当以形参传入，避免数据错乱
-		self._threadLock.acquire()
-		#i=0
-		#print(self._index)
-		#print(end)
-		for historyReqRep in allHistory[self._index:]:
-			# print("循环处理所有历史请求")
-			self.analyzeReqRep(historyReqRep)
-			# i = i+1
-			# if i==10:
-			# 	break
-		# 让起始位置变为最后一个请求的位置
-		self._index = end
-		self._threadLock.release()
-		#print(2222222222)
-		#print(json.dumps(self._allParas))
-		with open("allparas.json","w+") as f:
-			json.dump(self._allParas, f, ensure_ascii=False)
-			print "Files will be saved at " + os.getcwd() + "/allparas.json"
-		
-	def analyzeReqRep(self,historyReqRep):
-		# 获取域名
-		host = historyReqRep.getHttpService().getHost().encode('utf-8')
+    def getParas(self,end,allHistory):
+        # 会在线程中被改变的数需要在锁里面获取跟改变，如果以形参方式传入会导致数据错乱，不会改变的数据应当以形参传入，避免数据错乱
+        self._threadLock.acquire()
+        for historyReqRep in allHistory[self._index:]:
+            self.analyzeReqRep(historyReqRep)
+        # 让起始位置变为最后一个请求的位置
+        self._index = end
+        self._threadLock.release()
+        with open("allparas.json","w+") as f:
+            json.dump(self._allParas, f, ensure_ascii=False)
+            print "Files will be saved at " + os.getcwd() + "/allparas.json"
+        
+    def analyzeReqRep(self,historyReqRep):
+        # 获取域名
+        host = historyReqRep.getHttpService().getHost().encode('utf-8')
 
-		# print(host)
-		parasInfo = self._allParas.get(host)
-		if parasInfo == None:
-			parasInfo = {}
-		# 处理请求
-		analyzedRequest = self._helpers.analyzeRequest(historyReqRep)
-		
-		url = analyzedRequest.getUrl() # 获取java.net.URL对象
-		path = str(url.getPath())
-		if not path.endswith(".js") or not path.endswith(".png") or not path.endswith(".jpeg") or not path.endswith(".jpg") or not path.endswith(".css"):
-			# print(path)
-			paras1 = analyzedRequest.getParameters()
-			# print(len(paras1))
-			keyValues = parasInfo.get(path)
+        # print(host)
+        parasInfo = self._allParas.get(host)
+        if parasInfo == None:
+            parasInfo = {}
+        # 处理请求
+        analyzedRequest = self._helpers.analyzeRequest(historyReqRep)
+        
+        url = analyzedRequest.getUrl() # 获取java.net.URL对象
+        path = str(url.getPath())
+        if path.endswith(".js") or path.endswith(".png") or path.endswith(".jpeg") or path.endswith(".jpg") or path.endswith(".css"):
+        	return
+        else:
+            print(path)
+            paras1 = analyzedRequest.getParameters()
+            self.keyValues = parasInfo.get(path)
 
-			if keyValues == None:
-				keyValues = {}
-			for para in paras1:
-				try:
-					key = str(para.getName())
-					keyval = str(para.getValue())
-					#print(key+"==="+keyval)
+            if self.keyValues == None:
+                self.keyValues = {}
+            for para in paras1:
+                try:
+                    key = str(para.getName())
+                    keyval = str(para.getValue())
 
-					Values = keyValues.get(key)
+                    Values = self.keyValues.get(key)
 
-					if Values == None:
-						Values = []
-					if keyval not in Values:
-						Values.append(keyval)
-					keyValues[key] = Values
-				except:
-					continue
+                    if Values == None:
+                        Values = []
+                    if keyval not in Values:
+                        Values.append(keyval)
+                    self.keyValues[key] = Values
+                except:
+                    continue
 
-			# 处理响应
-			
-			response = historyReqRep.getResponse()
-			if response != None:
-				analyzedResponse = self._helpers.analyzeResponse(response)
-				# print(host)
-				if analyzedResponse.getInferredMimeType() == "JSON":
-					body = response[analyzedResponse.getBodyOffset():].tostring() # 获取返回包
-					jsonDict = json.loads(body).items() # 字典类型
-					for key,keyval in jsonDict:
-						#print(key)
-						new_key = "Rep_" + key
-						Values = keyValues.get(new_key)
-						if isinstance(keyval,unicode):
-							keyval = keyval.encode('utf-8')
-						else:
-							keyval = str(keyval)
-						#print(type(keyval))
-						if Values == None:
-							Values = []
-						if keyval not in Values:
-							Values.append(keyval)
+            # 处理响应
+            
+            response = historyReqRep.getResponse()
+            if response != None:
+                analyzedResponse = self._helpers.analyzeResponse(response)
+                if analyzedResponse.getInferredMimeType() == "JSON":
+                    body = response[analyzedResponse.getBodyOffset():].tostring() # 获取返回包
+                    self.parseJson(json.loads(body))
+            parasInfo[path] = self.keyValues
+            self._allParas[host] = parasInfo
 
-						keyValues[new_key] = Values
-						#print(Values)
-			parasInfo[path] = keyValues
-			self._allParas[host] = parasInfo
+        
+    def extensionUnloaded(self):
+        allHistory = self._callbacks.getProxyHistory()
+        self._end = len(allHistory)
+        end_t = Thread(target=self.getParas, args=(self._end,allHistory))
+        end_t.start()
 
-		
-	def extensionUnloaded(self):
-		print(11111)
-		allHistory = self._callbacks.getProxyHistory()
-		self._end = len(allHistory)
-		end_t = Thread(target=self.getParas, args=(self._end,allHistory))
-		end_t.start()
-
-
+    def parseJson(self,json):
+        keyvals = json.items()
+        length = len(keyvals)
+        index = 0
+        for key,val in keyvals:
+            index = index + 1
+            if isinstance(val,dict):
+                Values = self.keyValues.get(key)
+                if Values == None:
+                    Values = []
+                if "" not in Values:
+                	Values.append("")
+                self.keyValues[key] = Values
+                self.parseJson(val)
+            elif isinstance(val,list):
+                for listval in val:
+                    if isinstance(listval,dict):
+                        self.parseJson(listval)
+                    else:
+                        Values = self.keyValues.get(key)
+                        if Values == None:
+                            Values = []
+                        Values.append(listval)
+                        self.keyValues[key] = Values
+            else:
+                Values = self.keyValues.get(key)
+                if Values == None:
+                    Values = []
+                if val not in Values:
+                    Values.append(val)
+                self.keyValues[key] = Values
+                if length==index:
+                    return
