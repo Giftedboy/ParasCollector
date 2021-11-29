b8d08829258169d0321d8127a5f99c0fd0ede695
1ndex <2309896932@qq.com>
2021-11-29 21:10:15 +0800

    增加难看的UI界面

--- ParasCollectorV3.0.py
+++ ParasCollectorV3.0.py
@@ -7,11 +7,22 @@ from burp import IProxyListener
 from burp import IExtensionStateListener
 from burp import IContextMenuFactory
 from burp import IBurpExtenderCallbacks
+from burp import IMessageEditorController
+from burp import ITab
 import json
 import os
-
+import sys
 from javax.swing import JMenuItem
+from java.util import ArrayList;
+from javax.swing import JScrollPane;
+from javax.swing import JSplitPane;
+from javax.swing import JTabbedPane;
+from javax.swing import JTable;
+from javax.swing.table import AbstractTableModel;
 #from burp import IHttpRequestResponse
+sys.version[0] == '2':
+    reload(sys)
+    sys.setdefaultencoding("utf-8")# 老是会出现编码错误，所以设置一下编码
 print("""
   _____                     _____      _ _           _             
  |  __ \                   / ____|    | | |         | |            
@@ -25,24 +36,64 @@ print("""
  *************** https://www.cnblogs.com/wjrblogs/ ****************
 """)
 
+class LogEntry:
+    def __init__(self, host, paras):
+        self._host = host
+        self._paras = paras
 
+class Table(JTable):
+    def __init__(self, extender):
+        self._extender = extender
+        self.setModel(extender)
+    
+    def changeSelection(self, row, col, toggle, extend):
+    
+        # show the log entry for the selected row
+        logEntry = self._extender._log.get(row)
+        self._extender._parasViewer.setText(logEntry._paras)
+        # self._extender._currentlyDisplayedItem = logEntry._requestResponse
+        
+        JTable.changeSelection(self, row, col, toggle, extend)
 
-class BurpExtender(IBurpExtender, IProxyListener, IExtensionStateListener,IContextMenuFactory):
+class BurpExtender(IBurpExtender, IProxyListener, IExtensionStateListener,IContextMenuFactory, AbstractTableModel, IMessageEditorController, ITab):
     def registerExtenderCallbacks(self,callbacks):
         self._callbacks = callbacks
         self._helpers = callbacks.getHelpers()
         self._callbacks.setExtensionName('ParasCollector')
         # 记录扫描历史请求的位置，启用插件是默认为0
+        self._log = ArrayList()
         self._index = 0
         self._count = 0
         self._threadLock = Lock()
         self._allParas = {}
         self._i = 0
-        self._hostbalcklist = ["tracking.firefox.com.cn"] # 域名黑名单
+        self._hostbalcklist = ["tracking.firefox.com.cn","clack.7moor.com"] # 域名黑名单
         self._pathblacklist = ["/monitor_browser/collect/batch/",""] # 设置域名path黑名单，在该path下的将不收集，比方说心跳包，日志包，避免文件过大，脏数据问题
         callbacks.registerProxyListener(self)
         callbacks.registerExtensionStateListener(self)
         callbacks.registerContextMenuFactory(self)
+
+        # 主窗口
+        self._splitpane = JSplitPane(JSplitPane.VERTICAL_SPLIT)
+
+        logTable = Table(self)
+        scrollPane = JScrollPane(logTable)
+        self._splitpane.setLeftComponent(scrollPane)
+
+        # 详情
+        tabs = JTabbedPane()
+        self._parasViewer = callbacks.createTextEditor()
+        tabs.addTab("Paras",self._parasViewer.getComponent())
+        self._splitpane.setRightComponent(tabs)
+
+        # 定义 UI 组件
+        callbacks.customizeUiComponent(self._splitpane)
+        callbacks.customizeUiComponent(logTable)
+        callbacks.customizeUiComponent(scrollPane)
+        callbacks.customizeUiComponent(tabs)
+
+        # 将 UI 组件添加到 BURP 的 UI
+        callbacks.addSuiteTab(self)
 
         # 在启用插件的时候就扫描一次
         allHistory = self._callbacks.getProxyHistory()
@@ -72,15 +123,30 @@ class BurpExtender(IBurpExtender, IProxyListener, IExtensionStateListener,IConte
             self.analyzeReqRep(historyReqRep)
             self._i = self._i+1
         # 让起始位置变为最后一个请求的位置
+
         self._index = end
+        
         with open("allparas.json","w+") as f:
-            json.dump(self._allParas, f, ensure_ascii=False)
-            print "Files will be saved at " + os.getcwd() + "/allparas.json"
+            try:
+            	json.dump(self._allParas, f, ensure_ascii=False)
+            	print "Files will be saved at " + os.getcwd() + "/allparas.json"
+            except BaseException,err:
+            	print("file save filed,reason is:")
+            	print(err)
+            
+
+        row = self._log.size()
+        self._log.clear()
+        for host in self._allParas.keys():
+            self._log.add(LogEntry(host, json.dumps(self._allParas.get(host),sort_keys=True,indent=4,ensure_ascii=False)))
+            self.fireTableRowsInserted(row, row)
         self._threadLock.release()
+        
         
     def analyzeReqRep(self,historyReqRep):
         # 获取域名
         host = historyReqRep.getHttpService().getHost().encode('utf-8')
+        
         parasInfo = self._allParas.get(host)
         if parasInfo == None:
             parasInfo = {}
@@ -118,7 +184,10 @@ class BurpExtender(IBurpExtender, IProxyListener, IExtensionStateListener,IConte
                 analyzedResponse = self._helpers.analyzeResponse(response)
                 if analyzedResponse.getInferredMimeType() == "JSON":
                     body = response[analyzedResponse.getBodyOffset():].tostring() # 获取返回包
-                    self.parseJson(json.loads(body))
+                    try:
+                    	self.parseJson(json.loads(body))
+                    except:
+                    	print("Not a json")
             parasInfo[path] = self.keyValues
             self._allParas[host] = parasInfo
         
@@ -153,9 +222,9 @@ class BurpExtender(IBurpExtender, IProxyListener, IExtensionStateListener,IConte
                     except BaseException,err:
                         print(err)
                 else:
-                    print("path_noParas")
+                    print("path_noParas1")
             else:
-                print("host_noParas")
+                print("host_noParas2")
 
     def parseJson(self,json):
         keyvals = json.items()
@@ -198,3 +267,29 @@ class BurpExtender(IBurpExtender, IProxyListener, IExtensionStateListener,IConte
                 self.keyValues[key] = Values
                 if length==index:
                     return
+
+    def getTabCaption(self):
+        return "ParasCollector"
+    
+    def getUiComponent(self):
+        return self._splitpane
+
+    def getRowCount(self):
+        try:
+            return self._log.size()
+        except:
+            return 0
+
+    def getColumnCount(self):
+        return 1
+
+    def getColumnName(self, columnIndex):
+        if columnIndex == 0:
+            return "HOST"
+        return ""
+
+    def getValueAt(self, rowIndex, columnIndex):
+        logEntry = self._log.get(rowIndex)
+        if columnIndex == 0:
+            return logEntry._host
+        return ""
